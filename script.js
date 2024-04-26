ef6cd766acc6e018d9522fb76bbda9aa2770d805
petersonanacleto <petersonandrade77@gmail.com>
2024-04-25 22:17:13 -0300

    add checkbox in "realizarPostagem" file and update like button

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ document.getElementById('overlay').addEventListener('click', function(){
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('loginContainer').style.display = 'none';
 });
+
+document.getElementById('like').addEventListener('click', function(){
+    document.getElementById('icone-like').classList.replace('fa-regular', 'fa-solid');
+    document.getElementById('icone-like').style.color = '#2d44a0';
+});
