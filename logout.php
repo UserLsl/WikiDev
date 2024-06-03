d565d1dfa7f8dc34ad4282caa204c8d32ea85afa
UserLsl <lucassartorelli10@outlook.com>
2024-06-03 19:59:42 -0300

    fixed link to realizarPostagem.php in postagem.php

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
+    session_unset();
     session_destroy();
     //unset( $_SESSION['nome'] );
     //echo $_SESSION['nome'];
-    echo "<META HTTP-EQUIV=REFRESH CONTENT='0; URL=index.php'>";
+    echo "<META HTTP-EQUIV=REFRESH CONTENT='0; URL=index.php'>"; //header("Location: index.php"); //Faz a mesma coisa
--- postagem.php
+++ postagem.php
@@ -40,7 +40,7 @@
             echo "<ul class='dropdown'>
                     <li><a class='nome-usuario' href='#'>" . $_SESSION['nome'] . "<i class='fa-solid fa-angle-down'></i></a>
                         <ul>
-                            <li><a href='realizarPostagem.html'>Realizar Postagem</a></li>
+                            <li><a href='realizarPostagem.php'>Realizar Postagem</a></li>
                             <li><a href='logout.php' id='logout-btn'>
                                     Sair
                                 </a>
